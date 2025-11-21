6a6edae8eaea423b3e4b2065b655b1362a886087
Igor Zamiatin <igor.zamiatin@hireright.com>
2025-11-22 00:55:32 +0200

    Add JSON logging to middleware level

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -2,11 +2,15 @@ package handler
 
 import (
 	"cruder/internal/controller"
+	"cruder/internal/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func New(router *gin.Engine, userController *controller.UserController) *gin.Engine {
+	// Apply JSON logger middleware to all routes
+	router.Use(middleware.JSONLogger())
+
 	v1 := router.Group("/api/v1")
 	{
 		userGroup := v1.Group("/users")
