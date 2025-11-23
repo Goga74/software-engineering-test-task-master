123b7517a70d57dda9bcf1e074677048938759f8
Igor Zamiatin <igor.zamiatin@hireright.com>
2025-11-23 22:58:04 +0200

    Task4: add integration tests

--- internal/model/users.go
+++ internal/model/users.go
@@ -3,7 +3,7 @@ package model
 type User struct {
 	ID       int64  `json:"id"`
 	UUID     string `json:"uuid"` // Task3
-	Username string `json:"username"`
-	Email    string `json:"email"`
+	Username string `json:"username" binding:"required"` // Task4: validation added
+	Email    string `json:"email" binding:"required,email"` // Task4: validation added
 	FullName string `json:"full_name"`
 }
