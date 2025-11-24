59d015325f6ceaf272ce80960d3de3d299a4fe3f
Igor Zamiatin <igor.zamiatin@hireright.com>
2025-11-24 16:44:20 +0200

    Refactoring, fix fmt

--- internal/model/users.go
+++ internal/model/users.go
@@ -2,8 +2,8 @@ package model
 
 type User struct {
 	ID       int64  `json:"id"`
-	UUID     string `json:"uuid"` // Task3
-	Username string `json:"username" binding:"required"` // Task4: validation added
+	UUID     string `json:"uuid"`                           // Task3
+	Username string `json:"username" binding:"required"`    // Task4: validation added
 	Email    string `json:"email" binding:"required,email"` // Task4: validation added
 	FullName string `json:"full_name"`
 }
